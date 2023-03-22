8a3e5b8bc70370a3f52ebbe26cdcfa4068522055
me <adriandumitrascu.ro@gmail.com>
2023-03-22 21:57:07 +0100

    mobile version

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ import TinProfile from './Dog.js'
 
 const heroPost = new TinProfile(dogs[2])
 
+document.getEleme
+
 document.getElementById('hero-post').innerHTML = heroPost.getProfileHtml() 
+
 
 
 
