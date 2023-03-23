0c05345f5bcad33707ca54af3c97b25ad5562727
Adrian <adriandumitrascu.ro@gmail.com>
2023-03-23 19:56:55 +0100

    improve the ux

--- Dog.js
+++ Dog.js
@@ -1,24 +1,26 @@
-
-
-class TinProfile {
+class Dog {
     constructor(data){
-        Object.assign(this, data)
-        
+        Object.assign(this, data)  
+    }
+
+    setLikedSwiped = (like, swiped) => {
+        this.hasBeeLiked = like
+        this.hasBeenSwiped = swiped
     }
     
     getProfileHtml = () => {
         const {name, avatar, age, bio, hasBeenSwiped, hasBeeLiked} = this
         return `
-            <div class="badge"></div>
-            <div class="img-gradient">
-                <img id="hero-img" src=${avatar}>
-            </div>
-            <div class="hero-caption">
-                <h2>${name}, ${age}</h2>
-                <p>${bio}</p>
-            </div>
+                <div class="badge ${hasBeeLiked ? "like" : hasBeenSwiped ? "nope" : ""}"></div>
+                <div class="img-gradient">
+                    <img id="hero-img" src=${avatar}>
+                </div>
+                <div class="hero-caption">
+                    <h2>${name}, ${age}</h2>
+                    <p>${bio}</p>
+                </div>
            `
     }
 }
 
-export default TinProfile
+export default Dog
--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 import dogs from '/data.js'
-import TinProfile from './Dog.js'
+import Dog from './Dog.js'
 
 
+let index = 0
+let firstDog = new Dog(dogs[index])
+let isWaiting = false
+let isSwiped = false
+
+document.getElementById('heart-btn').addEventListener('click', like)
+document.getElementById('cross-btn').addEventListener('click', nope)
+
+function render () {
+  document.getElementById('hero-post').innerHTML = firstDog.getProfileHtml()
+} 
+
+render() 
 
 
-const heroPost = new TinProfile(dogs[2])
+function like() {
+  isSwiped = !isSwiped ? firstDog.setLikedSwiped(true, false) :
+  firstDog.setLikedSwiped(false, true)
+  render()
+  if (!isWaiting) {
+    isWaiting = true
+    setTimeout(() => {
+      index = index < 2 ? index+=1 : index = 0
+      firstDog = new Dog(dogs[index])
+      render()
+      isWaiting = false
+    }, 1500)
+  }
+}
 
-document.getEleme
-
-document.getElementById('hero-post').innerHTML = heroPost.getProfileHtml() 
-
+function nope() {
+  isSwiped = true
+  like()
+}  
 
 
 
